97ee82cc42e1275f2bad824652e7fead2d2f9b3b
e7217 <e7217@naver.com>
2018-06-04 11:32:05 +0900

    180604 1131

---
+++ rr_deviceName.py
@@ -0,0 +1,13 @@
+import pymodbus
+from pymodbus.client.sync import ModbusSerialClient as modclient
+from pymodbus.register_read_message import ReadInputRegistersRequest
+from pymodbus.register_read_message import ReadHoldingRegistersRequest
+
+
+client1 = modclient(method='rtu', port='/dev/ttyUSB0', stopbits=1, bytesize=8, baudrate=19200)
+
+conn = client1.connect()
+print(conn)
+
+valuehr = client1.read_holding_registers(256,1,unit=0x01)
+print valuehr.registers
---
+++ wr_powerOn.py
@@ -0,0 +1,15 @@
+import pymodbus
+from pymodbus.client.sync import ModbusSerialClient as modclient
+from pymodbus.register_read_message import ReadInputRegistersRequest
+from pymodbus.register_read_message import ReadHoldingRegistersRequest
+from pymodbus.framer.rtu_framer import ModbusRtuFramer
+
+
+client1 = modclient(method='rtu', port='/dev/ttyUSB0', stopbits=1, bytesize=8, baudrate=19200)
+
+conn = client1.connect()
+print(conn)
+
+rq = client1.write_register(257, 1, unit=0x01)
+rr = client1.read_holding_registers(257, 1, unit=0x01)
+print rr.registers
